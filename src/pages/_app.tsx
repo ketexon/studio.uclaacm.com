9b049b3f46a362ee8d66fe7e39d260bc6741bc44
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2023-07-09 23:54:22 -0700

    Added supabase and implemented add calendar event feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,23 @@ import "~/stylesheets/main.scss"
 
 import Layout from "~/components/Layout";
 
-export default function App({Component, pageProps}: AppPropsWithLayout){
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+	"https://ktdgdxrdzrfckapsuxiw.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0ZGdkeHJkenJmY2thcHN1eGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5NjYxODYsImV4cCI6MjAwNDU0MjE4Nn0.luQwNNM6RxSgb9xhwgRvShPMBukwaTuTEx0xDfcPPDM");
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
 	// optional layout function
 	const getLayout = Component.getLayout ?? (page => <Layout>{page}</Layout>);
 
 	return <>
-		<ThemeProvider theme={theme}>
-			<CssBaseline/>
-			{getLayout(<Component {...pageProps}/>)}
-		</ThemeProvider>
+		<SessionContextProvider supabaseClient={supabase}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				{getLayout(<Component {...pageProps} />)}
+			</ThemeProvider>
+		</SessionContextProvider>
 	</>
 }
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,14 +1,106 @@
 import * as React from "react";
+import { useState } from 'react';
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
+import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+
+import DateTimePicker from 'react-datetime-picker';
+
 type EventProps = {};
 
-export default function Events({}: EventProps) {
+export default function Events({ }: EventProps) {
+    const [start, setStart] = useState(new Date());
+    const [end, setEnd] = useState(new Date());
+    const [eventName, setEventName] = useState("");
+    const [eventDescription, setEventDescription] = useState("");
+
+    const session = useSession(); //tokens, when session exists, we have a user
+    const supabase = useSupabaseClient(); //talk to supabase
+    const { isLoading } = useSessionContext();
+
+    if (isLoading) {
+        return <></>
+    }
+
+    async function googleSignIn() {
+        const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                scopes: 'https://www.googleapis.com/auth/calendar'
+            }
+        });
+        if (error) {
+            alert("Error logging in to Google provider with Supabase");
+            console.log(error);
+        }
+    }
+
+    async function signOut() {
+        await supabase.auth.signOut();
+    }
+
+    async function createCalendarEvent() {
+        console.log("Created calendar event");
+        const event = {
+            'summary': eventName,
+            'description': eventDescription,
+            'start': {
+                'dateTime': start.toISOString(), // Date.toISOString() ->
+                'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+            },
+            'end': {
+                'dateTime': end.toISOString(), // Date.toISOString() ->
+                'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+            }
+        }
+        await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + session.provider_token //Access token for google
+            },
+            body: JSON.stringify(event)
+        }).then((data) => {
+            return data.json();
+        }).then((data) => {
+            console.log(data);
+            alert("Event created, check your Google Calendar");
+        })
+    }
+
+    console.log(session);
+    console.log(start);
+    console.log(end);
+    console.log(eventName);
+    console.log(eventDescription);
+
     return (
         <Box>
             <Typography variant="h1">Our events! </Typography>
+            <div style={{ width: "400px", margin: "30px auto" }}>
+                {session ?
+                    <>
+                        <h2>Hey there {session.user.email}</h2>
+                        <p>Start of your event</p>
+                        <DateTimePicker onChange={setStart} value={start} />
+                        <p>End of your event</p>
+                        <DateTimePicker onChange={setEnd} value={end} />
+                        <p>Event name</p>
+                        <input type="text" onChange={(e) => setEventName(e.target.value)} />
+                        <p>Event description</p>
+                        <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
+                        <hr />
+                        <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
+                        <p></p>
+                        <button onClick={() => signOut()}>Sign Out</button>
+                    </>
+                    :
+                    <>
+                        <button onClick={() => googleSignIn()}>Sign In With Google</button>
+                    </>
+                }
+            </div>
         </Box>
     );
 }
