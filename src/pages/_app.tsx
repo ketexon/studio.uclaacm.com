4779a0bb9b9463aecdf159d45d6bf5db9797a71f
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2023-07-10 21:36:53 -0700

    touched .env ;)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 const supabase = createClient(
 	"https://ktdgdxrdzrfckapsuxiw.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0ZGdkeHJkenJmY2thcHN1eGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg5NjYxODYsImV4cCI6MjAwNDU0MjE4Nn0.luQwNNM6RxSgb9xhwgRvShPMBukwaTuTEx0xDfcPPDM");
+	process.env.SUPABASE_PUBLIC_ANON_KEY);
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
 	// optional layout function
