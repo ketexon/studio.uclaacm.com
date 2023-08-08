6bd22ba2e073359249babb7de2bf45d8dd78b794
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2023-08-07 20:18:20 -0700

    added calendar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,20 +13,20 @@ import Layout from "~/components/Layout";
 import { createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
-const supabase = createClient(
+/*const supabase = createClient(
 	"https://ktdgdxrdzrfckapsuxiw.supabase.co",
-	process.env.SUPABASE_PUBLIC_ANON_KEY);
+	process.env.SUPABASE_PUBLIC_ANON_KEY);*/
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
 	// optional layout function
 	const getLayout = Component.getLayout ?? (page => <Layout>{page}</Layout>);
 
 	return <>
-		<SessionContextProvider supabaseClient={supabase}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				{getLayout(<Component {...pageProps} />)}
-			</ThemeProvider>
-		</SessionContextProvider>
+		{/*<SessionContextProvider supabaseClient={supabase}>*/}
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			{getLayout(<Component {...pageProps} />)}
+		</ThemeProvider>
+		{/*</SessionContextProvider>*/}
 	</>
 }
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -8,13 +8,63 @@ import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth
 
 import DateTimePicker from 'react-datetime-picker';
 
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import Divider from '@mui/material/Divider';
+
 type EventProps = {};
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+const useStyles = makeStyles((theme) => ({
+    shadowContainer: {
+        boxShadow: `0px 6px 6px -3px rgba(0,0,0,0.3),
+                  0px 2px 4px -1px rgba(0,0,0,0.1)`,
+    },
+}));
+
+function getLastSunday(curDate: Date) {
+    let year = curDate.getFullYear();
+    let month = curDate.getMonth();
+    let date = curDate.getDate();
+    let day = curDate.getDay();
+    return new Date(year, month, date - day);
+}
+
+function getCurMonth() {
+    let curDate = new Date();
+    curDate.setMonth(curDate.getMonth())
+    return curDate.toLocaleString('en-US', { month: 'long' })
+}
+
+function getCurYear() {
+    let curDate = new Date();
+    let year = curDate.getFullYear();
+    return year;
+}
+
+function getFirstDayOfMonth(curDate: Date): Date {
+    return new Date(curDate.getFullYear(), curDate.getMonth());
+}
+
 export default function Events({ }: EventProps) {
+    const classes = useStyles();
+    //return <div>Hello</div>;
+    /*React.useEffect(() => { console.log("hi") })
     const [start, setStart] = useState(new Date());
     const [end, setEnd] = useState(new Date());
     const [eventName, setEventName] = useState("");
     const [eventDescription, setEventDescription] = useState("");
+
 
     const session = useSession(); //tokens, when session exists, we have a user
     const supabase = useSupabaseClient(); //talk to supabase
@@ -73,12 +123,100 @@ export default function Events({ }: EventProps) {
     console.log(start);
     console.log(end);
     console.log(eventName);
-    console.log(eventDescription);
+    console.log(eventDescription);*/
 
+    const numRows = 6;
+    const numCols = 7;
+
+    console.log(getLastSunday(getFirstDayOfMonth(new Date())))
+    /*const renderCell = (rowIndex, colIndex) => {
+        let lastSunDate = getLastSunday(getFirstDayOfMonth(new Date()));
+        let currDate = new Date(lastSunDate.getFullYear(), lastSunDate.getMonth(),
+            lastSunDate.getDate() + 7 * rowIndex + colIndex);
+        return <div>{currDate.getDate()}</div>;
+    };*/
+
+    const renderCell = (index) => {
+        let lastSunDate = getLastSunday(getFirstDayOfMonth(new Date()));
+        let currDate = new Date(lastSunDate.getFullYear(), lastSunDate.getMonth(),
+            lastSunDate.getDate() + index);
+        return <div>{currDate.getDate()}</div>;
+    };
     return (
-        <Box>
+        <Container maxWidth="lg">
             <Typography variant="h1">Our events! </Typography>
-            <div style={{ width: "400px", margin: "30px auto" }}>
+
+            {/*<Grid container spacing={1}>
+                {Array.from({ length: numRows }, (_, rowIndex) => (
+                    <Grid key={rowIndex} container xs={12} spacing={1}>
+                {
+                    Array.from({ length: numCols }, (_, colIndex) => (
+                        <Grid key={colIndex} xs={1.5}>
+                            <Item>{renderCell(rowIndex, colIndex)}</Item>
+                        </Grid>
+                    ))
+                }
+                    </Grid>
+            </Grid > */}
+            <Container className={classes.shadowContainer}>
+                <Typography variant="h2" style={{ textAlign: 'center' }}>{getCurMonth() + " " + getCurYear()}</Typography>
+                <Divider variant="middle" />
+                <Grid container spacing={1} columns={14}>
+                    <Grid xs={2} >
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Sun</Item>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Mon</Item>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Tue</Item>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Wed</Item>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Thu</Item>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Fri</Item>
+                    </Grid>
+                    <Grid xs={2}>
+                        <Item sx={{
+                            color: '#ffffff',
+                            bgcolor: '#ed3266',
+                        }}>Sat</Item>
+                    </Grid>
+                </Grid >
+                <Grid container spacing={1} columns={14}>
+                    {Array.from({ length: numRows * numCols }).map((_, i) => (
+                        <Grid key={i} xs={2}>
+                            <Item elevation={0}
+                                sx={{
+                                    //color: 'success.main',
+                                }}>{renderCell(i)}</Item>
+                        </Grid>
+                    ))}
+                </Grid >
+
+                {/* <div style={{ width: "400px", margin: "30px auto" }}>
                 {session ?
                     <>
                         <h2>Hey there {session.user.email}</h2>
@@ -100,7 +238,8 @@ export default function Events({ }: EventProps) {
                         <button onClick={() => googleSignIn()}>Sign In With Google</button>
                     </>
                 }
-            </div>
-        </Box>
+            </div> */}
+            </Container>
+        </Container >
     );
 }
